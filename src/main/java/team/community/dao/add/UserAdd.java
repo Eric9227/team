af083bc4cfe70a9adf44aee1d5bb2e750bf1767c
ccccc <1257480643@qq.com>
2021-12-21 10:09:13 +0800

    wuhu

--- src/main/java/team/community/dao/add/UserAdd.java
+++ src/main/java/team/community/dao/add/UserAdd.java
@@ -1,5 +1,25 @@
 package team.community.dao.add;
 
-public class UserAdd {
+import team.community.bean.User;
+import team.community.controller.BaseServlet;
+import team.community.response.Resp;
+import team.community.util.JdbcUtil;
+
+import javax.servlet.annotation.WebServlet;
+@WebServlet("/user/add")
+public class UserAdd extends BaseServlet {
+    JdbcUtil jdbcUtil = new JdbcUtil();
+
+        @Override
+        protected void execute() {
+            // 接收参数
+            User user = parseParameter(User.class);
+            //写入数据库
+            String sql = "insert into user (getAccount,password)values(?,?)";
+
+            jdbcUtil.executeQuery(sql,User.class,user.getAccount(),user.getPassword());
+
+
+        }
 
 }

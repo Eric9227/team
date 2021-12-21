61c9d2e616c04e5af380796acfd4d41673b6b841
TAN00XU <TAN00XU@163.COM>
2021-12-21 13:56:27 +0800

    更改

---
+++ src/main/java/team/community/controller/message/MessageQueryByAccount.java
@@ -0,0 +1,25 @@
+package team.community.controller.message;
+
+import team.community.bean.Message;
+import team.community.bean.User;
+import team.community.controller.BaseServlet;
+import team.community.dao.query.MessageByAccount;
+
+import javax.servlet.annotation.WebServlet;
+import java.util.List;
+
+
+/**
+ * 发布的内容
+ * @author TAN00XU
+ */
+@WebServlet("/user/myMessage.json")
+public class MessageQueryByAccount extends BaseServlet {
+
+    @Override
+    protected void execute() {
+        User user = (User) request.getSession().getAttribute("user");
+        List<Message> messages = MessageByAccount.getMessageByAccount(user.getAccount());
+        responseJSON(messages);
+    }
+}
---
+++ src/main/java/team/community/dao/query/MessageByAccount.java
@@ -0,0 +1,21 @@
+package team.community.dao.query;
+
+import team.community.bean.Message;
+import team.community.util.JdbcUtil;
+
+import java.util.List;
+
+public class MessageByAccount {
+    private static JdbcUtil jdbcUtil = new JdbcUtil();
+
+    /**
+     * 根据账号查询发布的文章
+     * @param account
+     * @return
+     */
+    public static List<Message> getMessageByAccount(String account) {
+        String sql = "select * from message where account ?";
+        List<Message> messages = jdbcUtil.executeQuery(sql, Message.class, account);
+        return messages;
+    }
+}

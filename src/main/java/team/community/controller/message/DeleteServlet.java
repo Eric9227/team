c4471234176219373548c2757b73d6f9c58b4baa
TAN00XU <TAN00XU@163.COM>
2021-12-22 19:35:12 +0800

    更改

--- src/main/java/team/community/controller/message/DeleteServlet.java
+++ src/main/java/team/community/controller/message/DeleteServlet.java
@@ -1,8 +1,15 @@
 package team.community.controller.message;
 
+import lombok.SneakyThrows;
+import team.community.bean.User;
 import team.community.controller.BaseServlet;
+import team.community.dao.delete.MessageDelete;
 
 import javax.servlet.annotation.WebServlet;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Objects;
 
 /**
  * @author TAN00XU
@@ -10,12 +17,43 @@ import javax.servlet.annotation.WebServlet;
 @WebServlet("/DeleteServlet")
 public class DeleteServlet extends BaseServlet {
 
+    @SneakyThrows
     @Override
     protected void execute() {
-        System.out.println("作者的信息："+request.getParameter("author"));
-        System.out.println("发布时间："+request.getParameter("addTime"));
-      /*  if(request.getParameter("account") == session.getAttribute("user").getAccount()){
-            userService.delete(request.getParameter("account"),request.getParameter("add_time"));
-        }*/
+        String author = request.getParameter("author");
+        String addTime = request.getParameter("addTime");
+
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        LocalDateTime ldt = LocalDateTime.parse(addTime,df);
+
+        response.setContentType("text/html;charset=UTF-8");
+        request.setCharacterEncoding("UTF-8");
+        PrintWriter writer = response.getWriter();
+        if(Objects.equals(author, ((User) request.getSession().getAttribute("user")).getAccount())){
+            if(MessageDelete.delete(author,ldt)){
+                writer.println("""
+                    <script>
+                    alert("删除成功")
+                    location.href="/index.html";
+                    </script>
+                    """);
+            }else {
+                writer.println("""
+                    <script>
+                    alert("删除失败")
+                    location.href="/index.html";
+                    </script>
+                    """);
+            }
+
+
+        }else {
+            writer.println("""
+                    <script>
+                    alert("无权限删除")
+                    location.href="/index.html";
+                    </script>
+                    """);
+        }
     }
 }
